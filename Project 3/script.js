9edde3b603346a24f0bbe0dd6337abca1a49ee22
CarlosCampos10 <53207200+CarlosCampos10@users.noreply.github.com>
2019-10-28 16:37:19 -0700

    Update script.js

--- Project 3/script.js
+++ Project 3/script.js
@@ -1,260 +1,284 @@
-//makes focus go on name field first
-$("#name").focus();
-//hides the theme option
-$("#other-title").hide();
+let errorsCounter = 0;
+const nameValue = document.getElementById('name');
+const emailValue = document.getElementById('mail');
+const jobRole = document.getElementById('title');
+const otherTitle = document.getElementById('other-title');
 
-//shows or hides depending if other is selected or not
-$("#title").change(function() {
-  if ($("#title").val() === "other") {
-    $("#other-title").show();
-  } else {
-    $("#other-title").hide();
-  }
-});
+/** ****************************************
+Used for T-shirt Theme and Color Selection Section
+***************************************** */
+const designTheme = document.getElementById('design');
+const designsColors = document.querySelectorAll('#color option');
+const shirtColor = document.getElementById('color');
+const jsPuns = ['cornflowerblue', 'darkslategrey', 'gold'];
+const heartJS = ['tomato', 'steelblue', 'dimgrey'];
 
-//give user choice of color depending what design they chose
-let $colors = $("#color option");
-let colorOption = $colors.eq().text();
-$("#design").change(function() {
-  if ($("#design option:selected").val() === "js puns") {
-    $("#colors-js-puns").show();
-    $("#color").html(
-      '<option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option><option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option><option value="gold">Gold (JS Puns shirt only)</option>'
-    );
-  } else if ($("#design option:selected").val() === "heart js") {
-    $("#colors-js-puns").show();
-    $("#color").html(
-      '<option value="tomato">Tomato (I &#9829; JS shirt only)</option><option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option><option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option>'
-    );
-  }
-});
+/** ****************************************
+Used for fn. eventSelectorNode
+Which checks if there are any checkeventSelectorNode checked
+***************************************** */
+const activities = document.querySelector('fieldset.activities');
+const eventPrices = [
+  { name: 'all', price: 200, time: 'all' },
+  { name: 'js-frameworks', price: 100, time: 'tu9a' },
+  { name: 'js-libs', price: 100, time: 'tu1p' },
+  { name: 'express', price: 100, time: 'tu9a' },
+  { name: 'node', price: 100, time: 'tu1p' },
+  { name: 'build-tools', price: 100, time: 'w9a' },
+  { name: 'npm', price: 100, time: 'w1p' },
+];
+let cost = 0;
+const totalCost = document.createElement('p');
+activities.appendChild(totalCost);
+activities.lastChild.setAttribute('id', 'sum');
 
-//register for activities section
-let activitiesArray = [];
-let totalCost;
-const $activities = $("#activities");
-$(".activities").on("click", function() {
-  var total = 0;
-  if ($("input[name='all']").is(":checked")) {
-    total += 200; //adds up the cost if selected
-  }
-  if ($("input[name='js-frameworks']").is(":checked")) {
-    total += 100; //adds up the cost if selected
-    $("input[name='express']").attr("disabled", true);
-  } else {
-    $("input[name='express']").attr("disabled", false);
-  }
-  if ($("input[name='js-libs']").is(":checked")) {
-    total += 100; //adds up the cost if selected
-    $("input[name='node']").attr("disabled", true);
-  } else {
-    $("input[name='node']").attr("disabled", false);
-  }
-  if ($("input[name='express']").is(":checked")) {
-    total += 100; //adds up the cost if selected
-    $("input[name='js-frameworks']").attr("disabled", true);
-  } else {
-    $("input[name='js-frameworks']").attr("disabled", false);
-  }
-  if ($("input[name='node']").is(":checked")) {
-    total += 100; //adds up the cost if selected
-    $("input[name='js-libs']").attr("disabled", true);
-  } else {
-    $("input[name='js-libs']").attr("disabled", false);
-  }
+const submitButton = document.querySelector('[type="submit"]');
 
-  if ($("input[name=build-tools]").is(":checked")) {
-    total += 100; //adds up the cost if selected
-  }
-  if ($("input[name=npm]").is(":checked")) {
-    total += 100; //adds up the cost if selected
-  }
-  totalPrice(total);
-});
-// total price gets displayed
-function totalPrice(total) {
-  if (typeof total !== 0) {
-    $("#totalDiv").remove();
-    $(".activities").append("<div id='totalDiv'>Total : $" + total + "</div>");
+function setFocus(tag) {
+  const myElement = document.getElementById(tag);
+  myElement.focus();
+}
+
+setFocus('name');
+
+/** ****************************************
+Other-Title using Jquery
+***************************************** */
+$('#other-title').hide();
+jobRole.addEventListener('change', () => {
+  if (jobRole.value === 'other') {
+    $('#other-title').show();
+    setFocus('other-title');
   } else {
-    $("#totalDiv").remove();
+    $('#other-title').hide();
+  }
+}); // End jobRole Event
+
+/** ****************************************
+T-shirt Theme and Color Selection Section
+***************************************** */
+
+function themeSelector(state) {
+  const shirtNode = document.getElementById('color').firstElementChild;
+  const defaultColor = document.createElement('option');
+  const colorText = document.createTextNode('Please Select a Shirt Color');
+
+  if (state === true) {
+    defaultColor.setAttribute('selected', '');
+    defaultColor.setAttribute('value', 'default');
+    defaultColor.appendChild(colorText);
+    shirtColor.insertBefore(defaultColor, shirtNode);
   }
 }
 
-// credit card is selected first
-const creditCard = $("#credit-card");
+themeSelector(true);
 
-// shows and hides payment option according to users payment preference
-$("#payment").on("change", function() {
-  $(this)
-    .children()
-    .eq(0)
-    .hide();
-  if ($(this).val() === "credit card") {
-    creditCard.show();
-    payPal.hide();
-    bitcoin.hide();
+function colorSelector(what, display) {
+  for (let i = 0; i < what.length; i++) {
+    const whatColor = (document.querySelector(
+      `[value=${what[i]}]`
+    ).style.display = display);
   }
-  if ($(this).val() === "paypal") {
-    creditCard.hide();
-    payPal.show();
-    bitcoin.hide();
-  }
-  if ($(this).val() === "bitcoin") {
-    creditCard.hide();
-    payPal.hide();
-    bitcoin.show();
+} // End colorSelector
+
+$('#colors-js-puns').hide();
+
+designTheme.addEventListener('change', () => {
+  if (designTheme.value === 'js puns') {
+    $('#colors-js-puns').show();
+    colorSelector.call(this, heartJS, 'none');
+    colorSelector.call(this, jsPuns, 'block');
+    shirtColor.value = 'default';
+  } else if (designTheme.value === 'heart js') {
+    $('#colors-js-puns').show();
+    colorSelector.call(this, jsPuns, 'none');
+    colorSelector.call(this, heartJS, 'block');
+    shirtColor.value = 'default';
+  } else {
+    $('#colors-js-puns').hide();
   }
 });
 
-// Hide select payment method option
-$("#payment")[0].selectedIndex = $(
-  '#payment option[value = "credit card"]'
-).index();
+/** ****************************************
+CheckeventSelectorNode for Conference Events Selection
+Each events info is stored in eventPrices array
+Contains Custom realtime Validation
+***************************************** */
 
-//hides paypal and bitcoin initially
-const payPal = $("#credit-card").next();
-payPal.hide();
-const bitcoin = $("#credit-card")
-  .next()
-  .next();
-bitcoin.hide();
-
-// Main validation function
-const masterVal = () => {
-  if (isValidName() && isValidEmail() && checked() && isValidPayment()) {
-    return true;
+function eventSelectorNode() {
+  cost = 0;
+  for (const reset in eventPrices) {
+    const x = document.querySelector(`[name=${eventPrices[reset].name}]`);
+    x.removeAttribute('disabled'), x.parentNode.removeAttribute('class');
   }
-  return false;
-};
-// Main Validation event listener
-$("button").on("click", function(event) {
-  if (masterVal()) {
+
+  if (activities.querySelectorAll('input:checked').length === 0) {
+    console.log('No events have been selected'); // for testing
+    errorsCounter += 1; // marks that there is an error ie. no activity selected
+    const x = document.querySelector('.activities').firstElementChild;
+    const theSpan =
+      "<span class='line box'> (Select at least one activity)</span>";
+    x.innerHTML += theSpan;
   } else {
-    event.preventDefault();
-    nameVal();
-    emailVal();
-    checked();
-    cardVal();
-  }
-});
-
-//function for name
-const isValidName = () => {
-  const name = $("#name").val();
-  const nameCheck = /^[a-zA-Z ]+$/.test(name);
-  return nameCheck;
-};
-//validation of name
-const nameVal = () => {
-  if (isValidName()) {
-    $('label[for="name"]').css("color", "");
-    $("#name").css("border", "");
-  } else {
-    $('label[for="name"]').css("color", "red");
-    $("#name").css("border", "2px solid red");
-  }
-};
-$("#name").on("change", function() {
-  nameVal();
-});
-//function for email
-const isValidEmail = () => {
-  const email = $("#mail").val();
-  const emailCheck = /^.+@\w+\.com$/i.test(email);
-  return emailCheck;
-};
-//validation of email
-const emailVal = () => {
-  if (isValidEmail()) {
-    $('label[for="mail"]').css("color", "");
-    $("#mail").css("border", "");
-  } else {
-    $('label[for="mail"]').css("color", "red");
-    $("#mail").css("border", "2px solid red");
-  }
-};
-$("#mail").on("change", function() {
-  emailVal();
-});
-
-//check mark for the activities
-const checked = () => {
-  const activityCount = $(".activities input:checkbox:checked").length;
-  const activtyLegend = $(".activities legend");
-  if (activityCount >= 1) {
-    activtyLegend.css("color", "");
-    return true;
-  } else {
-    activtyLegend.css("color", "red");
-    return false;
-  }
-};
-
-$(".activities").on("change", function() {
-  checked();
-});
-
-//validating the credit card number, only 13-16 numbers allowed
-const isValidCC = () => {
-  const ccNumInput = $("#cc-num").val();
-  const ccNumCheck = /^[0-9]{13,16}$/.test(ccNumInput);
-  return ccNumCheck;
-};
-
-//fundtion for zip code, only 5 and/or4 numbers allowed
-const isValidZip = () => {
-  const zipCode = $("#zip").val();
-  const zipCodeCheck = /^\d{5}(?:[-\s]\d{4})?$/.test(zipCode);
-  return zipCodeCheck;
-};
-//function for CVV number, only 3 numbers allowed
-const isValidCVV = () => {
-  const cvv = $("#cvv").val();
-  const cvvCheck = /^[0-9]{3,4}$/.test(cvv);
-  return cvvCheck;
-};
-
-//validation of the credit card number, ccv number and zip code
-const cardVal = () => {
-  if (isValidCC()) {
-    $('label[for="cc-num"]').css("color", "");
-    $("#cc-num").css("border", "");
-  } else {
-    $('label[for="cc-num"]').css("color", "red");
-    $("#cc-num").css("border", "2px solid red");
-  }
-  if (isValidZip()) {
-    $('label[for="zip"]').css("color", "");
-    $("#zip").css("border", "");
-  } else {
-    $('label[for="zip"]').css("color", "red");
-    $("#zip").css("border", "2px solid red");
-  }
-  if (isValidCVV()) {
-    $('label[for="cvv"]').css("color", "");
-    $("#cvv").css("border", "");
-  } else {
-    $('label[for="cvv"]').css("color", "red");
-    $("#cvv").css("border", "2px solid red");
-  }
-};
-
-//event listener for credit card number, zip code and cvv number
-$("#cc-num, #zip, #cvv").on("change", function() {
-  cardVal();
-});
-
-//function for payment selected
-const isValidPayment = () => {
-  const paymentVal = $("#payment :selected").text();
-  if (paymentVal === "PayPal" || paymentVal === "Bitcoin") {
-    return true;
-  }
-  if (paymentVal === "Credit Card") {
-    if (isValidCC() && isValidZip() && isValidCVV()) {
-      return true;
+    const resetItems = document.querySelector(['.box']);
+    if (resetItems !== null) {
+      resetItems.remove();
+      errorsCounter -= 1;
     }
-    return false;
   }
-};
+
+  for (const checkbox in eventPrices) {
+    const x = document.querySelector(`[name=${eventPrices[checkbox].name}]`);
+    if (x.checked) {
+      const compare = eventPrices[checkbox].time;
+      cost += parseInt(eventPrices[checkbox].price);
+      for (const times in eventPrices) {
+        const other = eventPrices[times].time;
+        const y = document.querySelector(`[name=${eventPrices[times].name}]`);
+        if (y.checked === false) {
+          if (compare === other) {
+            y.setAttribute('disabled', '');
+            y.parentNode.setAttribute('class', 'disabled');
+            // Adds the CSS class disabled for custom styles
+          }
+        }
+      }
+    }
+  } // End of for loop
+  const totalFinal = document.getElementById('sum');
+  const theSum = document.createTextNode(`Total $ ${cost}`);
+  totalFinal.appendChild(theSum);
+  const message = `Total $ ${cost}`;
+  totalFinal.innerHTML = message;
+}
+eventSelectorNode();
+
+// EventListener for when checkeventSelectorNode changes
+activities.addEventListener('change', () => {
+  eventSelectorNode();
+  console.log('CheckeventSelectorNode Changed');
+});
+
+/** ****************************************
+ Payment Options Section
+***************************************** */
+
+const payment = document.getElementById('payment');
+const removeSelect = payment.firstElementChild;
+removeSelect.parentNode.removeChild(removeSelect);
+// sets Credit Card option as default
+document.querySelector('[value="credit card"]').setAttribute('selected', '');
+const creditCard = document.getElementById('credit-card');
+const payFieldset = document
+  .getElementById('payment')
+  .parentNode.querySelectorAll('fieldset>div');
+
+// used to hide payment options not selected
+function hidePaymentOptions(option, what) {
+  for (let i = 0; i < what.length; i++) {
+    if (i === option) {
+      what[i].style.display = 'block';
+    } else {
+      what[i].style.display = 'none';
+    }
+  }
+} // End hidePaymentOptions
+
+hidePaymentOptions(0, payFieldset);
+
+payment.addEventListener('change', () => {
+  if (payment.value === 'credit card') {
+    hidePaymentOptions(0, payFieldset);
+  } else if (payment.value === 'paypal') {
+    hidePaymentOptions(1, payFieldset);
+  } else {
+    hidePaymentOptions(2, payFieldset);
+  }
+});
+
+/** ****************************************
+ Start of Validation Section
+***************************************** */
+let message = 'Please correct the following field';
+
+function setError(id, message) {
+  id.setAttribute('class', 'error');
+  const x = document.querySelector(`[for= \'${id.id}\']`);
+  const theSpan = `<span class=\'line\'> (${message})</span>`;
+  x.innerHTML += theSpan;
+}
+
+// validates email
+function validEmail(check) {
+  return /^\w+@\w+(\.(\w)+)$/i.test(check.value);
+}
+
+// validates the credit card number
+function validate(num, bottom, top, message) {
+  const id = document.getElementById(num);
+  const testnum = id.value;
+  if (testnum === '' || isNaN(testnum)) {
+    setError(id, message);
+    errorsCounter += 1;
+  } else if (testnum.length < bottom || testnum.length > top) {
+    errorsCounter += 1;
+    setError(id, message);
+  } else {
+    console.log(`we have a valid ${num}!`);
+  }
+}
+
+submitButton.addEventListener('click', e => {
+  console.log(errorsCounter); // for testing and tracking
+
+  for (let i = 0; i < errorsCounter; i++) {
+    const resetItems = document.querySelector(['.line']);
+    // Below if(){}...Removes the error class from the parent element
+    if (document.querySelector('[class="error"]')) {
+      document
+        .getElementById(document.querySelector('[class="error"]').id)
+        .removeAttribute('class');
+    }
+    resetItems.remove();
+  }
+
+  errorsCounter = 0;
+
+  if (nameValue.value === '') {
+    errorsCounter += 1;
+    message = 'Please add a name';
+    setError(nameValue, message);
+  }
+  // Email section
+  if (!validEmail(emailValue)) {
+    errorsCounter += 1;
+    message = 'Please format email as follows: carlos.campos@gmail.com';
+    setError(emailValue, message);
+  }
+
+  if (activities.querySelectorAll('input:checked').length === 0) {
+    eventSelectorNode();
+  }
+  // Payment form
+  if (payment.value === 'credit card') {
+    const ccinfo = [
+      ['cc-num', 'zip', 'cvv'],
+      [13, 5, 3],
+      [16, 5, 3],
+      ['Enter 13 to 16 digit number', '5 Digits', '3 Digits'],
+    ];
+    for (let i = 0; i <= 2; i++) {
+      validate(ccinfo[0][i], ccinfo[1][i], ccinfo[2][i], ccinfo[3][i]);
+    }
+  }
+
+  if (errorsCounter > 0) {
+    e.preventDefault();
+    console.log(`THE NUMBER OF ERRORS: ${errorsCounter}`);
+    alert('Please correct the following fields.');
+    setFocus(document.querySelector('.error').id);
+  } else if (errorsCounter === 0) {
+    alert('Registration Complete!');
+  }
+});
+
